33d5e9161cfa68f899fca46ecc8b9ff4d9acb516
Nikolai Pawlowski <nikolaip@gmail.com>
2017-11-24 04:14:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Write your code in this file!
 function scuberGreetingForFeet(n) {
-  if (n < 400) {
-    return "This one is on me!"
-  }
-  else if (n > 2000) {
-    return "I will gladly take your thirty bucks."
-  }
+   switch(n) {
+    case n < 400:
+      return "This one is on me!"
+      break
+
+    }
+
 }
