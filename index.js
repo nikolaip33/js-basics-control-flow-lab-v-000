aae877f7f79c0b112e4a1e4558de89b7c6aef410
Nikolai Pawlowski <nikolaip@gmail.com>
2017-11-24 04:16:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Write your code in this file!
 function scuberGreetingForFeet(n) {
-   switch(n) {
+  let charge = ""
+  switch(n) {
     case n < 400:
-      return "This one is on me!"
+      charge "This one is on me!"
       break
 
     }
