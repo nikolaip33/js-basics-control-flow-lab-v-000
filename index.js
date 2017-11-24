fe82240608a7964588452a2f90dcbe2f60a73754
Nikolai Pawlowski <nikolaip@gmail.com>
2017-11-24 04:27:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code in this file!
+function scuberGreetingForFeet(n) {
+  if (n < 400) {
+    return "This one is on me!"
+  }
+  else if (n > 2500) {
+    return "No can do."
+  }
+  else if (n > 2000) {
+    return "I will gladly take your thirty bucks."
+  }
+}
+
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+}
+
+function switchOnCharmFromTip(n) {
+  switch(n) {
+    case 'generous':
+      return "Thank you so much."
+      break
+    case 'not as generous':
+      return "Thank you."
+      break
+    default:
+      return "Bye."
+      break
+  }
+}
