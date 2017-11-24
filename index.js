1642629308845f7f0bfebc3141c9fac24170566f
Nikolai Pawlowski <nikolaip@gmail.com>
2017-11-24 04:20:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ function scuberGreetingForFeet(n) {
   if (n < 400) {
     return "This one is on me!"
   }
-  else if (n > 2000) {
-    return "I will gladly take your thirty bucks."
-  }
   else if (n > 2500) {
     return "No can do."
   }
+  else if (n > 2000) {
+    return "I will gladly take your thirty bucks."
+  }
+
 }
